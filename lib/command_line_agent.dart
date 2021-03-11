a9056b32c2c750157f4bebaecbe9cf3ad832e1e0
AnhQuan Nguyen <j4qfrost@gmail.com>
2021-03-10 16:38:31 -0800

    add support for nullsafe code generation

--- lib/command_line_agent.dart
+++ lib/command_line_agent.dart
@@ -105,14 +105,15 @@ class ProjectAgent extends CommandLineAgent {
   }
 
   String _pubspecContents(
-      String name, Map<String, dynamic> deps, Map<String, dynamic> devDeps) {
+      String name, Map<String, dynamic> deps, Map<String, dynamic> devDeps,
+      {bool nullsafe = true}) {
     return """
 name: $name
 description: desc
 version: 0.0.1
 
 environment:
-  sdk: ">=2.0.0 <3.0.0"
+  sdk: ">=2.${nullsafe ? "12" : "0"}.0 <3.0.0"
 
 dependencies:
 ${_asYaml(deps, indent: 1)}
